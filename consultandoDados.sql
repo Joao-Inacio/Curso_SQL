39529ef500e9fe43256dadcf6b8bebb9d7f87de8
João Inácio <73400085+Joao-Inacio@users.noreply.github.com>
2022-08-24 18:20:58 -0300

    SELECT - Consultando dados

---
+++ consultandoDados.sql
@@ -0,0 +1,5 @@
+-- Active: 1661344300209@@127.0.0.1@3306@db_curso_web
+-- Consultando todas as colunas 
+SELECT * FROM tb_cursos;
+-- Selecionando pelo nomes da colunas
+SELECT nome_curso, data_cadastro, ativo, investimento, carga_horaria FROM tb_cursos;
