e95ffb659900c48b28ab82e6aa0743934e326fe1
João Inácio <73400085+Joao-Inacio@users.noreply.github.com>
2022-08-29 08:50:19 -0300

    SELECT - Filtros com Operadores de Comparação

---
+++ SelectOperadoresComparacao.sql
@@ -0,0 +1,5 @@
+-- Active: 1661344300209@@127.0.0.1@3306@db_curso_web
+-- Usando o operado de comparação
+SELECT * FROM tb_alunos WHERE interesse = 'Jogos';
+
+SELECT * FROM tb_alunos WHERE idade >= 18;
