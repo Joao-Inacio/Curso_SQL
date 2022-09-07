6a7dbb050f415e247349788db0032dfeff38cb61
João Inácio <73400085+Joao-Inacio@users.noreply.github.com>
2022-09-07 08:58:22 -0300

    UPDATE - Atualizando registros

---
+++ AtualizandoUPDATE.sql
@@ -0,0 +1,4 @@
+-- Active: 1661344300209@@127.0.0.1@3306@db_curso_web
+SELECT * FROM tb_alunos WHERE idade >= 80;
+UPDATE tb_alunos SET nome = 'João' WHERE id_aluno = 13;
+UPDATE tb_alunos SET interesse = 'Saúde' WHERE idade >= 80;
