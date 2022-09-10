e332a01798368af90949d1789bb922884dba4f98
Joao-Inacio <joaoinacio206@protonmail.com>
2022-09-10 18:55:28 -0300

    Criando o DB Projeto Loja Virtual

---
+++ Projeto_Loja_Virtual/01_criando_db_loja.sql
@@ -0,0 +1,7 @@
+-- Active: 1662843662404@@127.0.0.1@3306@db_loja_virtual
+-- Criando o Banco de Dado
+CREATE DATABASE db_loja_virtual;
+-- Cirando a tabela de produtos
+CREATE TABLE tb_produtos(
+    
+)
