c145a44aa1623d8407ec735ae40fb6a474ac6684
Joao-Inacio <joaoinacio206@protonmail.com>
2022-09-12 09:11:25 -0300

    Criando as tabelas de Produtos e descrição

--- Projeto_Loja_Virtual/01_criando_db_loja.sql
+++ Projeto_Loja_Virtual/01_criando_db_loja.sql
@@ -3,5 +3,14 @@
 CREATE DATABASE db_loja_virtual;
 -- Cirando a tabela de produtos
 CREATE TABLE tb_produtos(
-    
-)
+    id_produto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    produto VARCHAR(200) NOT NULL,
+    valor FLOAT(8,2) NOT NULL
+);
+-- Criando a tabela de informaçoes tecnicas
+CREATE TABLE tb_descricoes_tecnicas(
+    id_descricao_tecnica INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_produto INT NOT NULL,
+    descricao_tecnica TEXT NOT NULL,
+    FOREIGN KEY(id_produto) REFERENCES tb_produtos(id_produto)
+);
